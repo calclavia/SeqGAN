f58784f02ec7e8b6fe76ec226aa2effeabad06b8
Henry Mao <admin@calclavia.com>
2017-05-22 15:20:05 -0700

    Created simple generator model

--- models.py
+++ models.py
@@ -1,12 +1,30 @@
 from keras.models import Model
-from keras.layers import Input, Dense, LSTM, Activation
+from keras.layers import Input, Dense, LSTM, Embedding, Activation
 
 from constants import *
 
 def create_base_model():
+    """
+    Base model shared by generator and discriminator.
+    """
     seq_input = Input(shape=(SEQ_LEN,), dtype='int32')
 
     x = seq_input
+
+    x = Embedding(MAX_NB_WORDS, NUM_UNITS)(x)
     x = LSTM(NUM_UNITS)(x)
 
     return Model(seq_input, x)
+
+def create_generator(base_model):
+    """
+    Generator model
+    """
+    seq_input = Input(shape=(SEQ_LEN,), dtype='int32')
+
+    x = base_model(seq_input)
+
+    model = Model(seq_input, x)
+    model.compile(optimizer='nadam', loss='categorical_crossentropy')
+
+    return model
