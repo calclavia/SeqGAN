8493b5f5b42adf76e427aef1e9c8ae1c3947e0c4
Henry Mao <admin@calclavia.com>
2017-05-22 17:10:19 -0700

    Setup data pipeline

--- train.py
+++ train.py
@@ -7,23 +7,32 @@ from models import *
 from constants import *
 
 def load_data():
+    """
+    Loads training data from file and processes it.
+    """
+    print('Loading data...')
     fpath = 'data/obama.txt'
-    f = open(fpath, encoding='latin-1')
-    texts = f.read()
+
+    with open(fpath, encoding='utf-8') as f:
+        # Split large text into paragraphs
+        texts = f.read().split('\n\n')
 
     # Tokenize words
     tokenizer = Tokenizer(num_words=NUM_VOCAB)
     tokenizer.fit_on_texts(texts)
     sequences = tokenizer.texts_to_sequences(texts)
 
-    word_index = tokenizer.word_index
-    print('Found %s unique tokens.' % len(word_index))
+    print('Found {} unique tokens.'.format(len(tokenizer.word_index)))
 
-    data = pad_sequences(sequences, maxlen=SEQ_LEN)
+    # Create training data and target data.
+    # Target data is training data shfited by one word
+    train_data = pad_sequences(sequences[:][:-1], maxlen=SEQ_LEN)
+    target_data = pad_sequences(sequences[:][1:], maxlen=SEQ_LEN)
 
-    labels = to_categorical(np.asarray(labels))
-    print('Shape of data tensor:', data.shape)
-    print('Shape of label tensor:', labels.shape)
+    assert train_data.shape == target_data.shape
+
+    print('Shape of train_data/target_data tensor:', train_data.shape)
+    return train_data, target_data
 
 def main():
     load_data()
