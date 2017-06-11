Fixed reward normalization

p_model.py
@@ -102,7 +102,13 @@ class Generator(nn.Module):
 
     def reinforce(self, outputs, rewards):
         optimizer = optim.Adam(self.parameters())
-        advantages = rewards
+
+        # Compute advantages
+        rewards = rewards.data.cpu().numpy()
+        rewards -= np.mean(rewards)
+        rewards /= np.std(rewards)
+        advantages = Variable(torch.Tensor(rewards)).cuda()
+
         loss = -torch.sum(torch.mm(outputs, advantages))
         loss.backward()
         optimizer.step()
@@ -153,4 +159,4 @@ class Discriminator(nn.Module):
         loss.backward()
         optimizer.step()
 
-        return output, loss.data[0] / input_seqs.size()[0], accuracy.data[0]
+        return loss.data[0] / input_seqs.size()[0], accuracy.data[0]

p_train.py
@@ -26,26 +26,30 @@ def main():
     run_d_acc = None
 
     max_iterations = 100000
-    gan_iteration = 10000
+    gan_iteration = 20000
     t = tqdm(range(max_iterations))
 
     for i in t:
+        if np.random.random() < i / gan_iteration:
+            # Train generator (REINFORCE)
+            fake_text, outputs = generator.sample(batch=batch_size, length=SEQ_LEN, eval_mode=False)
+            input_seqs = Variable(input_tensors(fake_text)).cuda()
+            rewards, _ = discriminator(input_seqs, None)
+            generator.reinforce(outputs, rewards)
+        else:
+            # Train generator (MLE)
+            input_seqs, target_seqs = make_g_batch(text)
+            g_loss = generator.train_step(input_seqs, target_seqs)
+            run_g_loss = g_loss if run_g_loss is None else run_g_loss * 0.99 + g_loss * 0.01
+
         # Train discriminator
-        fake_text, outputs = generator.sample(batch=batch_size // 2, length=SEQ_LEN, eval_mode=False)
+        fake_text, outputs = generator.sample(batch=batch_size // 2, length=SEQ_LEN)
         input_seqs, target_seqs = make_d_batch(fake_text, text)
-        pred, d_loss, d_acc = discriminator.train_step(input_seqs, target_seqs)
+        d_loss, d_acc = discriminator.train_step(input_seqs, target_seqs)
 
         run_d_loss = d_loss if run_d_loss is None else run_d_loss * 0.99 + d_loss * 0.01
         run_d_loss = d_loss if run_d_loss is None else run_d_loss * 0.99 + d_loss * 0.01
         run_d_acc = d_acc if run_d_acc is None else run_d_acc * 0.99 + d_acc * 0.01
-
-        # Train generator (REINFORCE)
-        generator.reinforce(outputs, pred[:batch_size // 2])
-
-        # Train generator (MLE)
-        input_seqs, target_seqs = make_g_batch(text)
-        g_loss = generator.train_step(input_seqs, target_seqs)
-        run_g_loss = g_loss if run_g_loss is None else run_g_loss * 0.99 + g_loss * 0.01
 
         # Track loss
         t.set_postfix(g_loss=run_g_loss, d_loss=run_d_loss, d_acc=run_d_acc)